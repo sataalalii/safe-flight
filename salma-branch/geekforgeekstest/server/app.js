5e0218b9852a035f44c3308e8ce5a206672d49a9
sataalalii <sataalalii@gmail.com>
2022-11-10 10:32:28 -0500

    node and react connected, no search bar yet.

--- salma-branch/geekforgeekstest/server/app.js
+++ salma-branch/geekforgeekstest/server/app.js
@@ -5,6 +5,11 @@ app.get("/", (req, res) => {
 res.send("Hello World!");
 });
 
+app.post("/post", (req, res) => {
+    console.log("connected to React");
+    res.redirect("/");
+});
+
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
