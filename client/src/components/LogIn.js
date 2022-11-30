7621c5be7a808a56c31056cfa05896a44f5bf6df
Ekaterina Azarova <eazarova@bu.edu>
2022-11-30 18:13:56 -0500

    moved stuff around

--- client/src/components/LogIn.js
+++ client/src/components/LogIn.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import Axios from 'axios';
+import {auth} from '../server.js'
 
 class LogIn extends Component {
     constructor(props) {
@@ -42,7 +43,7 @@ class LogIn extends Component {
                 <input type="text" name = "email" placeholder="email address" onChange={e => this.handleChange(e)} />
                 <input type="text" name = "password" placeholder="password"  onChange={e => this.handleChange(e)}/>
                 <input type="submit" value="submit" onClick={this.handleFormSubmit}/>
-                <input type="button" value="Log in with Google" onClick={auth()}/>
+                <input type="submit" value="Log in with Google" onClick={auth()}/>
                 {/*<input type="submit" value="clear form" onClick={this.handleClearForm}/>*/}
             </form>
         );
@@ -51,104 +52,3 @@ class LogIn extends Component {
 
 export default LogIn;
 
-//passport routing
-function auth(){
-    const express = require('express');
-const app = express();
-const passport = require('passport');
-const cookieSession = require('cookie-session');
-require('./passport');
-  
-app.use(cookieSession({
-    name: 'google-auth-session',
-    keys: ['key1', 'key2']
-}));
-app.use(passport.initialize());
-app.use(passport.session());
-    
-/*
-//button 
-app.get('/', (req, res) => {
-    res.send("<button><a href='/auth'>Login With Google</a></button>")
-});
-*/
-
-// Auth 
-app.get('/auth' , passport.authenticate('google', { scope:
-    [ 'email', 'profile' ]
-}));
-  
-
-// Auth Callback
-app.get( '/auth/callback',
-    passport.authenticate( 'google', {
-        successRedirect: '/auth/callback/success',
-        failureRedirect: '/auth/callback/failure'
-}));
-  
-// Success 
-app.get('/auth/callback/success' , (req , res) => {
-    if(!req.user)
-        res.redirect('/auth/callback/failure');
-    res.send("Welcome " + req.user.email);
-});
-  
-// failure
-app.get('/auth/callback/failure' , (req , res) => {
-    res.send("Error");
-});
-  
-app.listen(4000 , () => {
-    console.log("Server Running on port 4000");
-});
-}
-/*
-const express = require('express');
-const app = express();
-const passport = require('passport');
-const cookieSession = require('cookie-session');
-require('./passport');
-  
-app.use(cookieSession({
-    name: 'google-auth-session',
-    keys: ['key1', 'key2']
-}));
-app.use(passport.initialize());
-app.use(passport.session());
-    
-
-//button 
-app.get('/', (req, res) => {
-    res.send("<button><a href='/auth'>Login With Google</a></button>")
-});
-
-
-// Auth 
-app.get('/auth' , passport.authenticate('google', { scope:
-    [ 'email', 'profile' ]
-}));
-  
-
-// Auth Callback
-app.get( '/auth/callback',
-    passport.authenticate( 'google', {
-        successRedirect: '/auth/callback/success',
-        failureRedirect: '/auth/callback/failure'
-}));
-  
-// Success 
-app.get('/auth/callback/success' , (req , res) => {
-    if(!req.user)
-        res.redirect('/auth/callback/failure');
-    res.send("Welcome " + req.user.email);
-});
-  
-// failure
-app.get('/auth/callback/failure' , (req , res) => {
-    res.send("Error");
-});
-  
-app.listen(4000 , () => {
-    console.log("Server Running on port 4000");
-});
-*/
--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const bodyparse = require('body-parser');
 const app = express();
 
 
+
 // app.use(cors);
 app.use(bodyparse.json());
 app.use(bodyparse.urlencoded({extended: true}));
@@ -28,3 +29,54 @@ app.use((req, res) => {
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+
+//passport routing
+function auth(){
+    const passport = require('passport');
+const cookieSession = require('cookie-session');
+require('./passport');
+app.use(cookieSession({
+    name: 'google-auth-session',
+    keys: ['key1', 'key2']
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+    
+/*
+//button 
+app.get('/', (req, res) => {
+    res.send("<button><a href='/auth'>Login With Google</a></button>")
+});
+*/
+
+// Auth 
+app.get('/auth' , passport.authenticate('google', { scope:
+    [ 'email', 'profile' ]
+}));
+  
+
+// Auth Callback
+app.get( '/auth/callback',
+    passport.authenticate( 'google', {
+        successRedirect: '/auth/callback/success',
+        failureRedirect: '/auth/callback/failure'
+}));
+  
+// Success 
+app.get('/auth/callback/success' , (req , res) => {
+    if(!req.user)
+        res.redirect('/auth/callback/failure');
+    res.send("Welcome " + req.user.email);
+});
+  
+// failure
+app.get('/auth/callback/failure' , (req , res) => {
+    res.send("Error");
+});
+  
+app.listen(3000 , () => {
+    console.log("Server Running on port 3000");
+});
+}
+
