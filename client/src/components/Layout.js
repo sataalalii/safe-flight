156301d7b10cbc3b929f17522a633c64291b077a
kevkimbu <kevkim@bu.edu>
2022-11-30 15:12:17 -0500

    Added detailed navbar and fixed page displaying.

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -1,25 +1,39 @@
-import { Outlet, Link } from "react-router-dom";
+import {Outlet} from "react-router-dom";
+import {
+    Nav,
+    NavLink,
+    Bars,
+    NavMenu,
+    NavBtn,
+    NavBtnLink
+  } from './NavbarElements';
 
-const Layout = () => {
+  const Layout = () => {
     return (
-        <>
-            <nav>
-                <ul>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/login">Log in or sign up</Link>
-                    </li>
-                    <li>
-                        <Link to="/contact">Contact</Link>
-                    </li>
-                </ul>
-            </nav>
-
-            <Outlet />
-        </>
-    )
-};
+      <>
+        <Nav>
+          <NavLink to='/Home'>
+            <img src={require('../assets/logo.png')} alt='logo' />
+          </NavLink>
+          <Bars />
+          <NavMenu>
+            <NavLink to='/About' activeStyle>
+              About
+            </NavLink>
+            <NavLink to='/Contact' activeStyle>
+              Contact
+            </NavLink>
+            {/* Second Nav */}
+            {/* <NavBtnLink to='/sign-in'>Sign In</NavBtnLink> */}
+          </NavMenu>
+          <NavBtn>
+            <NavBtnLink to='/LogIn'>Log In</NavBtnLink>
+          </NavBtn>
+        </Nav>
+        <Outlet />
+      </>
+      
+    );
+  };
 
 export default Layout;
