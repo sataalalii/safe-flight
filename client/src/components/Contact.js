c913958902dfe6f0e1514288c0c55524941904ad
kevkimbu <kevkim@bu.edu>
2022-12-09 00:44:38 -0500

    Added flags to the locations using emoji-js import

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -1,9 +1,16 @@
 import React from "react";
+var EmojiConvertor = require('emoji-js');
+var emoji = new EmojiConvertor();
+
+// Flags that are not supported by the emoji-js library
+emoji.addAliases({
+    'ne' : '1f1f3-1f1f1'
+  });
 
 const Contact = () => {
     return (
         <div>
-            <h1>Contact Us Page</h1>
+            <h1>{emoji.replace_colons(":doge:")}</h1>
         </div>
     );
 };
